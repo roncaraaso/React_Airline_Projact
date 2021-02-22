f352373b2ca0887dee5a83c59b2c1c24106fed11
roncaraaso <78846965+roncaraaso@users.noreply.github.com>
2021-02-22 19:42:56 +0200

    Add files via upload

---
+++ Models/SEtRedis.cs
@@ -0,0 +1,43 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class SetRedis
+    {
+        
+        public static int airlineNum = 4;
+        public static bool setToRedis( string host , string key , string value)
+        {
+            bool success = false;
+            using (RedisClient client = new RedisClient(host))
+            {
+                
+                if(client.Get<string>(key) == null){
+                    success = client.Set(key, value);
+                }
+            }
+            return success;
+        }
+
+        public static string  Get(string host ,string key)
+        {
+            using(RedisClient client = new RedisClient(host))
+            {
+                return client.Get<string>(key);
+               // return client.GetAll<string>();
+            }
+        }
+        public static void Remove(string host, string key)
+        {
+            using (RedisClient client = new RedisClient(host))
+            {
+               client.Del(key);
+               
+            }
+        }
+    }
+}
---
+++ Models/SendGrid.cs
@@ -0,0 +1,26 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class SendGridServ
+    {
+        public static void Execute( string fromEmail , string toEmail , string toUser, string content ,string htmlcont)
+        {
+            
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(fromEmail, "Admin");
+            var subject = "New mail from Admin";
+            var to = new EmailAddress(toEmail, toUser);
+            var plainTextContent = $"{content}";
+            var htmlContent = $"<strong>{htmlcont}</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+    }
+}
